b30e3c2def91e795f2044f30dd4a9659f772b53e
chayounglee <chayounglee@gmail.com>
2017-12-14 17:52:05 +0900

    Step20_InputOutput committed

---
+++ Step20_InputOutput/src/test/main/MainClass03.java
@@ -0,0 +1,24 @@
+﻿package test.main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MainClass03 {
+	public static void main(String[] args) {
+		InputStream is = System.in;
+		InputStreamReader isr = new InputStreamReader(is);
+		
+		BufferedReader br = new BufferedReader(isr);
+		try {
+			System.out.println("한줄 입력:");
+			String line = br.readLine();
+			System.out.println("=입력한 문자열 =");
+			System.out.println(line);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ Step20_InputOutput/src/test/main/MainClass04.java
@@ -0,0 +1,29 @@
+﻿package test.main;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class MainClass04 {
+	public static void main(String[] args) throws IOException {
+		OutputStream os = System.out;
+		os.write(97);
+		os.write(98);
+		os.write(99);
+		os.write(100);
+		
+		//방출해서 출력하기.
+		os.flush();
+		
+		byte[] buffer= {65, 66, 67, 68};
+		//byte[] 객체를 출력할 준비
+		os.write(buffer);
+		//출력
+		os.flush();
+		//스트림 닫기
+		os.close();
+		//닫은 후에는 출력 불가
+		os.write(69);
+		
+		System.out.println("메인 메소드가 종료됩니다.");
+	}
+}
